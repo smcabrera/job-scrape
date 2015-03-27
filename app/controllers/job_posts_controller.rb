acd0e716649b71dc3890dcf9d93bbd2a87309595
Stephen Mariano Cabrera <stephen.m.cabrera@gmail.com>
2015-03-27 14:33:57 -0500

    Ajaxify archive action

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -16,6 +16,7 @@ class JobPostsController < ApplicationController
   end
 
   def archived
+    # TODO: this shouldn't exist...you want a view with a scope defined not another controller action
     @job_posts = JobPost.only_deleted
   end
 
@@ -42,10 +43,13 @@ class JobPostsController < ApplicationController
 
     if @job_post.destroy
       flash[:notice] = "Job post was archived. Check in the archive to restore the post"
-      redirect_to job_posts_path
     else
       flash[:error] = "There was a problem deleting this job post. Please try again."
-      redirect_to job_posts_path
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 end
